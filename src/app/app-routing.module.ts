5b9fc689e509407980dbfe0869aa0d25405e7164
Hannes Limmer <holzuhren-test@gmx.de>
2022-10-28 23:34:23 +0200

    added primeflex primeicons and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DatabaseContributionComponent } from './database-contribution/database-contribution.component';
+import { TranslatorComponent } from './translator/translator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'wissen-beitragen',
+    component: DatabaseContributionComponent,
+  },
+  {
+    path: '',
+    component: TranslatorComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/translator/translator.component.ts
+++ src/app/translator/translator.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, NonNullableFormBuilder } from '@angular/forms';
+
+type TranslatorFormType = FormGroup<{
+  germanText: FormControl<string>;
+  bavarianText: FormControl<string>;
+}>;
 
 @Component({
   selector: 'app-translator',
   templateUrl: './translator.component.html',
-  styleUrls: ['./translator.component.scss']
+  styleUrls: ['./translator.component.scss'],
 })
-export class TranslatorComponent implements OnInit {
+export class TranslatorComponent {
+  public translatorForm: TranslatorFormType = this._fb.group({
+    germanText: this._fb.control<string>(''),
+    bavarianText: this._fb.control<string>(''),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(private _fb: NonNullableFormBuilder) {}
 }
