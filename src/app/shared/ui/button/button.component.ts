094affeefa0a1f9ee7f557c8f2421829ed95e277
Hannes Limmer <holzuhren-test@gmx.de>
2022-10-29 18:22:58 +0200

    improved layout and added send button

--- src/app/shared/ui/button/button.component.ts
+++ src/app/shared/ui/button/button.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+const buttonType = ['button', 'submit', 'reset'] as const;
+const buttonStyle = [
+  'success',
+  'secondary',
+  'info',
+  'warning',
+  'help',
+  'danger',
+] as const;
+
+type ButtonType = typeof buttonType[number];
+type ButtonStyle = typeof buttonStyle[number];
 
 @Component({
-  selector: 'app-button',
+  selector: 'app-button[label]',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
+  @Input() form?: FormGroup | FormControl;
+  @Input() disabled = false;
+  @Input() label!: string;
+  @Input() type: ButtonType = 'button';
+  @Input() style?: ButtonStyle;
 
-  constructor() { }
+  @Output() clickEvent: EventEmitter<Event> = new EventEmitter<Event>();
 
-  ngOnInit(): void {
+  public isDisabled(): boolean {
+    return (
+      this.disabled ||
+      (this.form
+        ? !this.form.valid || (this.form.valid && !this.form.dirty)
+        : false)
+    );
   }
-
 }
