d378ff73c47dc793e04216183f6417162824de2c
Hannes Limmer <holzuhren-test@gmx.de>
2022-10-30 02:19:42 +0200

    added SEO

--- src/app/pages/translator/translator.component.ts
+++ src/app/pages/translator/translator.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, NonNullableFormBuilder } from '@angular/forms';
 import { takeUntil } from 'rxjs/operators';
 import { Destroyable } from 'src/app/shared/classes/destroyable';
 import { PostTranslationsService } from 'src/app/shared/services/api/post-translations.service';
+import { Meta, Title } from '@angular/platform-browser';
 
 type TranslatorFormType = FormGroup<{
   germanText: FormControl<string>;
@@ -24,9 +25,25 @@ export class TranslatorComponent extends Destroyable {
 
   constructor(
     private _fb: NonNullableFormBuilder,
-    private _postTranslationService: PostTranslationsService
+    private _postTranslationService: PostTranslationsService,
+    private _meta: Meta,
+    private _title: Title
   ) {
     super();
+    this._meta.addTags([
+      {
+        name: 'description',
+        content:
+          'Deutsch - Bairisch (Bayerisch/Bayrisch) Übersetzer! Auch für Österreich geeignet!',
+      },
+      { name: 'author', content: 'Johannes Limmer' },
+      {
+        name: 'keywords',
+        content:
+          'Bairisch Übersetzer, Bayerisch Übersetzer, Bayrisch Übersetzer',
+      },
+    ]);
+    this._title.setTitle('Bairisch Übersetzer');
   }
 
   public sendTranslationRequest() {
